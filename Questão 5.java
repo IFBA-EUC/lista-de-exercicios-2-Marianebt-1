3a550a65a8f4a1309c7f1a97bc0f1b3fb081820c
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-08-19 17:14:22 -0300

    Add files via upload
    Questão 5

---
+++ Questão 5.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Questão 5
+{
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++){
+		    System.out.println("Tabuada. de "+ i);
+		    for (int j = 1; j <= 10; j++) {
+		        System.out.println(i + " x " + j + " = " + (i * j));
+		    }
+		}
+	}
+}

dcf655396fe11c23ad6da2d42de2a00c52e41aec
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-08-24 17:40:58 -0300

    Add files via upload
    Questão 15

---
+++ Questão 15.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Questão 15
+{
+    public static void main(String[] args) {
+	}
+    public double calmedia(double m1, double m2, double m3) {
+        return m1+m2+m3/3;
+        double media;
+        
+    }
+    public String conceito () {
+        return "Aprovado"
+        if media <= 6,0;
+        return "Verificação Suplementar"
+        if media <= 4,0 e >= 6,0;
+        return "Reprovado"
+        if media > 4,0;
+    }
+	
+}

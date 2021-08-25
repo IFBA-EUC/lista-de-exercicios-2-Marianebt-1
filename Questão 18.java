b252657a0802023d0902d99f4d1d74eaacedd068
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-08-25 10:06:43 -0300

    Add files via upload
    Questão 18

---
+++ Questão 18.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Questão 18
+{
+	public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+	int[] array = {1,2,3,4,5,6,7,8,9};
+	String[] numeros = new String [2];
+	}
+}

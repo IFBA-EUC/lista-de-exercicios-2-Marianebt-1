093c283d96466ffeedf7839f3e539b24a8bc8779
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-08-25 10:25:49 -0300

    Add files via upload
    Questão 21

---
+++ Questão 21.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Questão 21
+public String Empréstimo() {
+    return "O livro só pode ficar com você até 8 dias";
+public String Livro() {
+    return "O livro deve ser devolvido imtacto";
+public String Pessoa() {
+    return "A pessoa deve cuidar bem do livro";
+}
+}
+}

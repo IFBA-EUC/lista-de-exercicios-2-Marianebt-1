6872dc284e0e23b8a01c685458312f979fdb006a
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-08-25 10:07:11 -0300

    Add files via upload
    Questão 20

---
+++ Questão 20.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	   
+System.out.println("Digite o nome:");
+System.out.println("Digite o telefone:");
+private String nome;
+private int telefone;
+
+public String getnome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String gettelefone() {
+	return telefone;
+}
+
+public void setTelefone(String telefone) {
+	this.telefone = telefone;
+}
+
+
